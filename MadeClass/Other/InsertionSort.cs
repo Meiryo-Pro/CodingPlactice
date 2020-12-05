8b422c39e79cbb0bb85f19e9e52fb6e757150abe
meisoft <meisoft.p.q@gmail.com>
2020-12-05 21:31:46 +0900

    挿入ソートとクイックソートのクラスを追加

---
+++ MadeClass/Other/InsertionSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.Other
+{
+    class InsertionSort
+    {
+        private const int ListCount = 10;
+        public static void Main(string[] args)
+        {
+            var insertionSort = new InsertionSort();
+            var list = new List<int>(ListCount);
+            for (var i = 1; i <= ListCount; i++)
+            {
+                list.Add(i);
+            }
+
+            var randomList = new List<int>(list.OrderBy(i => Guid.NewGuid()).ToList<int>());
+            LinkedList<int> sortedList = insertionSort.Sort(randomList);
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("ソート後");
+            insertionSort.Display(sortedList);
+        }
+
+        public LinkedList<int> Sort(List<int> randomList)
+        {
+            Console.WriteLine("ソート前");
+            Display(randomList);
+            Console.WriteLine("----------------");
+            Console.WriteLine("ソート中");
+
+            var list = new LinkedList<int>(randomList);
+            for (var node = list.First; node.Next != null; node = node.Next)
+            {
+                while (node.Value > node.Next.Value)
+                {
+                    var temp = node.Next.Value;
+                    list.Remove(node.Next);
+                    list.AddBefore(node, temp);
+
+                    if (node.Previous.Previous != null)
+                    {
+                        node = node.Previous.Previous;
+                    }
+                    Display(list);
+                }
+            }
+
+            return list;
+        }
+
+        public void Display(ICollection<int> list)
+        {
+            foreach (var v in list)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ MadeClass/Other/QuickSort.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.Other
+{
+    class QuickSort
+    {
+        private const int ListCount = 10;
+        public static void Main(string[] args)
+        {
+            var q = new QuickSort();
+            var list = new List<int>(ListCount);
+            for (var i = 1; i <= ListCount; i++)
+            {
+                list.Add(i);
+            }
+
+            list = list.OrderBy(i => Guid.NewGuid()).ToList<int>();
+            Console.WriteLine("ソート前");
+            q.Display(list);
+            Console.WriteLine("----------------");
+            Console.WriteLine("交換中");
+
+            q.Sort(list, 0, list.Count - 1);
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("ソート後");
+            q.Display(list);
+        }
+
+        public void Sort(List<int> list, int left, int right)
+        {
+            
+            if(left >= right)
+            {
+                return;
+            }
+
+            var l = left;
+            var r = right - 1;
+            var pivot = list[right];
+
+            while (true)
+            {
+                while (list[l] < pivot)
+                {
+                    l++;
+                }
+
+                while (l < r && pivot < list[r])
+                {
+                    r--;
+                }
+
+                if (l >= r)
+                {
+                    break;
+                }
+
+                Swap(list, l, r);
+                Display(list);
+            }
+
+            Swap(list, l, right);
+            Display(list);
+
+            Sort(list, left, l - 1);
+            Sort(list, l + 1, right);
+        }
+
+        public void Swap(List<int> list, int left, int right)
+        {
+            var temp = list[left];
+            list[left] = list[right];
+            list[right] = temp;
+        }
+
+        public void Display(ICollection<int> list)
+        {
+            foreach (var v in list)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
